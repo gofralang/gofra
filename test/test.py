556bb650610814abc9052e30afcf065f44d70b55
kirillzhosul <79853674+kirillzhosul@users.noreply.github.com>
2021-11-08 12:44:21 +0400

    A bit more tests

--- test/test.py
+++ test/test.py
@@ -42,12 +42,13 @@ TEST_EXTENSION = ".mspl"
 
 # Directories to run test on.
 TEST_DIRECTORIS = [
-    "../examples/"
+    ("../examples/", "/examples/"),
+    ("../tests/", "/tests/")
 ]
 
 for test_directory in TEST_DIRECTORIS:
     # Iterate over test directories.
-    for test_file in os.listdir(test_directory):
+    for test_file in os.listdir(test_directory[0]):
         # Iterate over test files.
 
         # Continue if not test extension.
@@ -55,7 +56,7 @@ for test_directory in TEST_DIRECTORIS:
             continue
 
         # Base command for CLI.
-        cli_execute_path = test_directory + test_file
+        cli_execute_path = test_directory[0] + test_file
         cli_base_command = ["python", LANG_PATH, cli_execute_path]
 
         # Run commands.
@@ -69,15 +70,17 @@ for test_directory in TEST_DIRECTORIS:
         run_result_current = run_result.stdout.decode("utf-8")
         run_result_current = run_result_current.encode("unicode_escape").decode("utf-8")
 
+        # Get record path.
+        record_file_path = TEST_RECORDS_DIRECTORY + test_directory[1] + os.path.basename(cli_execute_path) + ".txt"
+
         if RECORD_NEW:
             # If we record.
 
             # Write result.
-            record_file_path = TEST_RECORDS_DIRECTORY + os.path.basename(cli_execute_path) + ".txt"
             with open(record_file_path, "w") as record_file:
                 record_file.write(run_result_current)
         else:
-            record_file_path = TEST_RECORDS_DIRECTORY + os.path.basename(cli_execute_path) + ".txt"
+            # Read expected result.
             with open(record_file_path, "r") as record_file:
                 run_result_expected = "".join(record_file.readlines())
 
